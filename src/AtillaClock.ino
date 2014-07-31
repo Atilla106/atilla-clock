fabb219cae48713f31e0af66730686b6a4536e5f
Flavien Raynaud <raynaudfla@eisti.eu>
2014-07-31 16:40:33 +0200

    Time is checked every 2 minutes. See if this works during the holidays. Then put it to refresh every hour.

--- src/AtillaClock.ino
+++ src/AtillaClock.ino
@@ -38,124 +38,119 @@ EthernetUDP Udp;
 unsigned int last_check = 0;
 #define HOUR_ADD 2
 
+const unsigned long seventyYears = 2208988800UL;
+
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+    Serial.begin(115200);
+    while (!Serial);
 
-  Wire.begin();
+    Wire.begin();
 
-  randomSeed(analogRead(1));
+    randomSeed(analogRead(1));
 
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
 
-  /* LED MATRICES */
-  ht1632c.setup(HT1632C_BRIGHTNESS);
+    /* LED MATRICES */
+    ht1632c.setup(HT1632C_BRIGHTNESS);
 
-  /* RTC MODULE */
-  ds1307.begin(); //start RTC Clock
+    /* RTC MODULE */
+    ds1307.begin(); //start RTC Clock
 
-  Serial.println(__DATE__);
-  Serial.println(__TIME__);
+    Serial.println(__DATE__);
+    Serial.println(__TIME__);
 
-  if (!ds1307.isrunning()) {
-    Serial.println(F("RTC is NOT running!"));
-    ds1307.adjust(DateTime(__DATE__, __TIME__));
-  }
+    if (!ds1307.isrunning()) {
+        Serial.println(F("RTC is NOT running!"));
+        ds1307.adjust(DateTime(__DATE__, __TIME__));
+    }
 
-  DateTime now = ds1307.now();
-  clock.hour = now.hour();
-  clock.minute = now.minute();
+    DateTime now = ds1307.now();
+    clock.hour = now.hour();
+    clock.minute = now.minute();
 
-  /* SD CARD */
+    /* SD CARD */
 
-  pinMode(10, OUTPUT);
+    pinMode(10, OUTPUT);
 
-  if (!SD.begin(4)) {
-    Serial.println(F("SD Card cannot be initialized!"));
-    return; // We're fucked up
-  }
+    if (!SD.begin(4)) {
+        Serial.println(F("SD Card cannot be initialized!"));
+        return; // We're fucked up
+    }
 
-  font.print_large_clock_number(11, 0, clock.hour);
-  font.print_large_colon(23, 0);
-  font.print_large_clock_number(27, 0, clock.minute);
+    font.print_large_clock_number(11, 0, clock.hour);
+    font.print_large_colon(23, 0);
+    font.print_large_clock_number(27, 0, clock.minute);
 
-  Serial.println(F("Starting ethernet..."));
-  Ethernet.begin(mac, ip);
-  Udp.begin(localPort);
-  Serial.println(F("Done ethernet..."));
+    Serial.println(F("Starting ethernet..."));
+    Ethernet.begin(mac, ip);
+    Udp.begin(localPort);
+    Serial.println(F("Done ethernet..."));
 
-  Serial.println(F("Everything is working!"));
+    Serial.println(F("Everything is working!"));
 }
 
 void loop() {
-  DateTime now = ds1307.now();
+    DateTime now = ds1307.now();
 
-  if (now.minute() != clock.minute) {
-    clock.minute = now.minute();
-    font.print_large_clock_number(27, 0, clock.minute);
+    if (now.minute() != clock.minute) {
+        clock.minute = now.minute();
+        font.print_large_clock_number(27, 0, clock.minute);
 
-    if (Transition::transition("RED.CLK", ht1632c)) {
-      ht1632c.clear();
-      font.print_large_clock_number(11, 0, clock.hour);
-      font.print_large_clock_number(27, 0, clock.minute);
-    }
-  }
-
-  if (now.hour() != clock.hour) {
-    clock.hour = now.hour();
-    font.print_large_clock_number(11, 0, clock.hour);
-  }
-
-  /* if (last_check != now.hour()) {
-    Serial.println(F("Go..."));
-    sendNTPpacket(timeServer);
-    Serial.println(F("..."));
-
-    if (Udp.parsePacket()) {
-      Serial.println(F("Starting..."));
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);
-
-      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-      unsigned long secsSince1900 = highWord << 16 | lowWord;
-      const unsigned long seventyYears = 2208988800UL;
-      unsigned long epoch = secsSince1900 - seventyYears;
-
-      uint8_t h = (HOUR_ADD + (epoch % 86400L) / 3600) % 24;
-      uint8_t m = (epoch % 3600) / 60;
-      uint8_t s = epoch / 60;
-
-      ds1307.adjust(DateTime(now.year(), now.month(), now.day(), h, m, s));
-      Serial.println(ds1307.now().minute());
-      last_check = h;
+        if (Transition::transition("RED.CLK", ht1632c)) {
+            ht1632c.clear();
+            font.print_large_clock_number(11, 0, clock.hour);
+            font.print_large_clock_number(27, 0, clock.minute);
+        }
     }
 
-    Serial.println("Done.");
-  } */
+    if (now.hour() != clock.hour) {
+        clock.hour = now.hour();
+        font.print_large_clock_number(11, 0, clock.hour);
+    }
+
+    if (abs(now.minute() - last_check) > 2) {
+        Serial.println(F("Go..."));
+        sendNTPpacket(timeServer);
+        Serial.println(F("..."));
+
+        delay(1000);
+        
+        if (Udp.parsePacket()) {
+            Serial.println(F("Starting..."));
+            Udp.read(packetBuffer, NTP_PACKET_SIZE);
+
+            unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+            unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+            unsigned long secsSince1900 = highWord << 16 | lowWord;
+            unsigned long epoch = secsSince1900 - seventyYears;
+
+            uint8_t h = (HOUR_ADD + (epoch % 86400L) / 3600) % 24;
+            uint8_t m = (epoch % 3600) / 60;
+            uint8_t s = epoch % 60;
+
+            ds1307.adjust(DateTime(now.year(), now.month(), now.day(), h, m, s));
+            Serial.println(ds1307.now().minute());
+            last_check = m;
+        }
+
+        Serial.println("Done.");
+    } 
 }
 
 // Send an NTP request to the time server at the given address 
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  packetBuffer[0] = 0b11100011;
-  packetBuffer[1] = 0;
-  packetBuffer[2] = 6;
-  packetBuffer[3] = 0xEC;
-  packetBuffer[12] = 49; 
-  packetBuffer[13] = 0x4E;
-  packetBuffer[14] = 49;
-  packetBuffer[15] = 52;
+unsigned long sendNTPpacket(IPAddress& address) {
+    memset(packetBuffer, 0, NTP_PACKET_SIZE);
+    packetBuffer[0] = 0b11100011;
+    packetBuffer[1] = 0;
+    packetBuffer[2] = 6;
+    packetBuffer[3] = 0xEC;
+    packetBuffer[12] = 49; 
+    packetBuffer[13] = 0x4E;
+    packetBuffer[14] = 49;
+    packetBuffer[15] = 52;
 
-  Udp.beginPacket(address, 123);
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket(); 
-}
-
-static uint8_t conv2d(const char* p) {
-    uint8_t v = 0;
-    if ('0' <= *p && *p <= '9')
-        v = *p - '0';
-    return 10 * v + *++p - '0';
+    Udp.beginPacket(address, 123);
+    Udp.write(packetBuffer, NTP_PACKET_SIZE);
+    Udp.endPacket(); 
 }
